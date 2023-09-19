cb63b7aa1d98e3cb145759784d0c8a4709e5d09e
Darylcosm0 <mutaidaryl@gmail.com>
2023-09-19 10:33:11 +0300

    :feat: added sidenav icons

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Timeline.css'
+import Suggestions from '../suggestions/Suggestions'
 
 function Timeline() {
   return (
-    <div>Timeline</div>
+    <div className="timeline">
+        <div className="timeline__left">Timeline</div>
+        {/* <div className="timeline__right">Suggestions</div> */}
+        <Suggestions/>
+    </div>
   )
 }
 
