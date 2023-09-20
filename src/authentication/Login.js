df5c0b619676cf1083c41ebb2a781e601e91632b
Darylcosm0 <mutaidaryl@gmail.com>
2023-09-20 11:05:39 +0300

    feat: Implement email and password authentication in Login component.

--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import './Login.css'
+import  { useState } from "react";
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
-}
-
-export default Login
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const handleLogin = () => {
+      signInWithEmailAndPassword(auth, email, password);
+    };
+    return (
+      <div className="login">
+        <img
+          src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
+          alt=""
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button onClick={handleLogin}>Log in</button>
+      </div>
+    );
+  }
+  
+  export default Login;
