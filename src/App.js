e987f5fa131ad23ae272df4acca25509b64cdb6a
Darylcosm0 <mutaidaryl@gmail.com>
2023-09-25 16:03:37 +0300

    🐛 Fix: fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Homepage from "./Homepage";
-import Authenticate from "./authenticate/Authenticate";
-import { useDispatch, useSelector } from "react-redux";
+import Authenticate from './authentication/Authentication';
+import { useDispatch, useSelector } from  'react-redux';
 import { useEffect } from "react";
 import { auth } from "./firebase";
 import { loginUser, setLoading } from "./features/userSlice";
